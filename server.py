2d9271b803f3269e7a1ef26bedbce1654252b11e
Jon Leichter <jon@Jons-MacBook-Air.local>
2018-04-16 17:00:13 +0800

    Iniital checkin

---
+++ server.py
@@ -0,0 +1,34 @@
+from flask import Flask, flash, session, request, redirect, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/planes')
+def planes():
+    return render_template('planes.html')
+
+@app.route('/trains')
+def trains():
+    return render_template('trains.html')
+
+@app.route('/automobiles')
+def automobiles():
+    return render_template('automobiles.html')
+
+@app.route('/boats')
+def boats():
+    return render_template('boats.html')
+
+@app.route('/account')
+def account():
+    return render_template('account.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+app.run(debug=True)
+
